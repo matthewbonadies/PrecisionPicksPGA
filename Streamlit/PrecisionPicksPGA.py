e04d491028bae891fede4fe970d5bea8f73339f7
Matthew Bonadies <mattbon06@gmail.com>
2024-04-13 17:46:46 -0400

    code updates

--- Streamlit/PrecisionPicksPGA.py
+++ Streamlit/PrecisionPicksPGA.py
@@ -1,7 +1,6 @@
 ### import libraries
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import statsmodels.api as sm
 import plotly.express as px
 from datetime import datetime, timedelta
