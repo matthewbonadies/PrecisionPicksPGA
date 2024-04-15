1648dcbfc15f023670bde3c7f8329807b988c034
Matthew Bonadies <mattbon06@gmail.com>
2024-04-15 13:30:08 -0400

    code update st

--- Streamlit/PrecisionPicksPGA.py
+++ Streamlit/PrecisionPicksPGA.py
@@ -28,7 +28,6 @@ from keras.callbacks import ModelCheckpoint
 from keras.models import load_model
 from sklearn.metrics import r2_score
 import streamlit as st
-from keras.layers import TFSMLayer
 import joblib
 
 def home_page():
@@ -40,10 +39,10 @@ def home_page():
         return df
 
     #######################################################################################################################################
-    train_df_sorted = load_data('./data/train_df_sorted.csv')
-    test_df_sorted = load_data('./data/test_df_sorted.csv')
-    player_df = load_data('./data/player_df.csv')
-    field_df = load_data('./data/field_df.csv')
+    train_df_sorted = load_data('../data/train_df_sorted.csv')
+    test_df_sorted = load_data('../data/test_df_sorted.csv')
+    player_df = load_data('../data/player_df.csv')
+    field_df = load_data('../data/field_df.csv')
 
     #######################################################################################################################################
     # Drop the non-numeric and non-lagged columns prior to training our model
@@ -73,8 +72,8 @@ def home_page():
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
     X_test_scaled = scaler.transform(X_test)
-    @st.cache(allow_output_mutation=True)
-    model = keras.models.load_model('pga_nn_1', call_endpoint='serving_default')
+    # @st.cache(allow_output_mutation=True)
+    model = keras.models.load_model('pga_nn_1')
 
     #######################################################################################################################################
     sorted_df = predict_df_sorted.sort_values(by=['dg_id', 'round_completed'])
@@ -157,7 +156,7 @@ def stats_page():
         df = pd.read_csv(filedf)
         return df    
     
-    total_df_sorted = load_data('./data/total_df_sorted.csv')
+    total_df_sorted = load_data('../data/total_df_sorted.csv')
     total_df_sorted = total_df_sorted.sort_values(by='round_completed', ascending=True)
     
     total_df_sorted['round_completed'] = pd.to_datetime(total_df_sorted['round_completed'])
@@ -205,7 +204,7 @@ st.sidebar.text("")
 def load_data(filedf):
         df = pd.read_csv(filedf)
         return df
-rank_df = load_data('./data/rank_df.csv')
+rank_df = load_data('../data/rank_df.csv')
 st.sidebar.title("World Golf Rankings")
 rank_df = rank_df.sort_values(by='owgr_rank', ascending=True)
 st.sidebar.dataframe(rank_df)
