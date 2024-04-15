589d53691ab0ac496a5a21c947bedafa55d2e1b0
Matthew Bonadies <mattbon06@gmail.com>
2024-04-15 13:10:48 -0400

    st update

--- Streamlit/PrecisionPicksPGA.py
+++ Streamlit/PrecisionPicksPGA.py
@@ -73,8 +73,8 @@ def home_page():
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
     X_test_scaled = scaler.transform(X_test)
-
-    model = TFSMLayer('pga_nn_1', call_endpoint='serving_default')
+    @st.cache(allow_output_mutation=True)
+    model = keras.models.load_model('pga_nn_1', call_endpoint='serving_default')
 
     #######################################################################################################################################
     sorted_df = predict_df_sorted.sort_values(by=['dg_id', 'round_completed'])
