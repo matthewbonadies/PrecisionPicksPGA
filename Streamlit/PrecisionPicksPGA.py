1eae382d33eaef32647f19c9300cc2a014ea411b
Matthew Bonadies <mattbon06@gmail.com>
2024-04-17 19:02:39 -0400

    version updates

--- Streamlit/PrecisionPicksPGA.py
+++ Streamlit/PrecisionPicksPGA.py
@@ -78,7 +78,7 @@ st.sidebar.title("Welcome to PrecisionPicksPGA")
 x=0
 def home_page():
     st.write("TensorFlow version:", tf.__version__)
-    st.write("Keras version:", keras.__version__)
+    # st.write("Keras version:", keras.__version__)
     global x
 
     st.title("Tournament Prediction Model")
